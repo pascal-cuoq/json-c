2b9ba4671a1f8610861f5291f264e666bcd4630e
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-06-12 15:01:18 +0200

    Fix: linkhash.h 2

--- linkhash.h
+++ linkhash.h
@@ -50,7 +50,12 @@ extern int tis_lh_empty;
 /**
  * sentinel pointer value for freed slots
  */
+#ifdef __TRUSTINSOFT_BUGFIX__
+extern int tis_lh_freed;
+#define LH_FREED (void *)(&tis_lh_freed)
+#else
 #define LH_FREED (void *)-2
+#endif
 
 /**
  * default string hash function
